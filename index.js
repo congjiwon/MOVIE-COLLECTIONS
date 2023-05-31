7729c46b62792a5e706ab761d14bd7acff05acc1
congjiwon <sheepg1510@gmail.com>
2023-05-31 12:40:47 +0900

    영화card 클릭 시 영화id값 alert 창으로 띄우기

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let movies = [];
 const cardWrapper = document.getElementById("card-wrapper");
-const cardWrapper123 = document.querySelector(".card");
+const card = document.querySelector(".card");
 
 const options = {
   method: "GET",
@@ -17,7 +17,7 @@ fetch(
 )
   .then((response) => response.json())
   .then((response) => {
-    let arr = response.results;
+    let arr = response.results; //tmdb에서 api 받아와서 arr에 넣어주기
     console.log(arr);
 
     arr.forEach((a) => {
@@ -29,10 +29,11 @@ fetch(
 
       console.log(poster_path);
 
-      let temp_html = `<div class="card">
+      let temp_html = `<div class="card" onclick="alert(${id})">
                               <img src="https://image.tmdb.org/t/p/w200${poster_path}" alt="" />
                               <h3 id="title">${title}</h3>
                               <p id="score">score: ${vote_average}</p>
+                              <p id="movieId" style="display:none">${id}</p>
                               <p id="explanation">${overview}</p>
                         </div>`;
 
@@ -40,3 +41,13 @@ fetch(
     });
   })
   .catch((err) => console.error(err));
+
+document.querySelector(".card").addEventListener("click", (event) => {
+  alert("dd");
+});
+
+// card.onclick = showID;
+// const showID = function () {
+//   alert();
+// };
+//card 클릭 -> alert 창 띄우기
